62f616da3060855253def38afd2e310679a49498
Miguel Angel <m.zapes@gmail.com>
2015-05-28 23:52:58 +0200

    Adding Basic Files
    Setting the controller

---
+++ site/controller.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_workteam
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or Later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die(Restricted access);
+
+/**
+ * WorkTeam Component Controller
+ *
+ * @since 0.0.1
+ */
+class WorkTeamController extends JControllerLegacy
+{
+   
+}
