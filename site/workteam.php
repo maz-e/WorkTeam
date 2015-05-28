644b83c967a500a9e4246bfab990ab50afa67385
Miguel Angel <m.zapes@gmail.com>
2015-05-28 19:42:40 +0200

    Adding Basic Files

---
+++ site/workteam.php
@@ -0,0 +1,21 @@
+<? php
+/**
+* @package     Joomla.Administrator
+* @subpackage  com_workteam
+*
+* @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+* @license     GNU General Public License version 2 or Later; see LICENSE.txt
+*/
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Get an instance of the controller prefixed by WorkTeam
+$controller = JControllerLegacy::getInstance('WorkTeam');
+
+// Perform the request task
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
