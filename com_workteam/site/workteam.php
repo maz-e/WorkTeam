c070ee193aeca6db6c777be5601c70ec3c265f8e
Miguel Angel <m.zapes@gmail.com>
2015-05-29 13:28:16 +0200

    Corrected mistakes

--- com_workteam/site/workteam.php
+++ com_workteam/site/workteam.php
@@ -1,11 +1,11 @@
-<? php
+<?php
 /**
-* @package     Joomla.Administrator
-* @subpackage  com_workteam
-*
-* @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
-* @license     GNU General Public License version 2 or Later; see LICENSE.txt
-*/
+ * @package     Joomla.Administrator
+ * @subpackage  com_workteam
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or Later; see LICENSE.txt
+ */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
