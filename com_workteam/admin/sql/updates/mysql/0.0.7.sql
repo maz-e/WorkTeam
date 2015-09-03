7fb0a397d67fe81472339ef15b8e8eaa6e559642
Miguel Angel <m.zapes@gmail.com>
2015-09-03 13:33:36 +0200

    Edit database code mistake

--- com_workteam/admin/sql/updates/mysql/0.0.7.sql
+++ com_workteam/admin/sql/updates/mysql/0.0.7.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `#__helloworld` ADD `contact_name` VARCHAR(60) NOT NULL,
-ALTER TABLE `#__helloworld` DROP `name` int(11) NOT NULL DEFAULT '0';
-ALTER TABLE `#__helloworld` DROP `surname` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `#__helloworld` DROP `name`;
+ALTER TABLE `#__helloworld` DROP `surname`;
